290da6d571592f2ecd971bbd7cfcdb369e5aeca9
Erik Poot Sánchez <erikpootsanchez@gmail.com>
2022-11-17 22:50:16 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,72 @@
 <script setup>
-    import List from "./components/List.vue";
+import { RouterLink, RouterView } from 'vue-router'
+import SingIn from './views/SignIn.vue'
 </script>
 
 <template>
-    <List />
+  <SingIn/>
 </template>
 
-<style>
-    body {
-        font-family: system-ui, BlinkMacSystemFont, -apple-system, Segoe UI, Roboto,
-        Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    }
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
 
-    #app {
-        padding: 2rem;
-        margin: 1rem;
-        border-radius: 3px;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08);
-        width: 95%;
-        max-width: 900px;
-        margin: 0 auto;
-    }
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
 </style>
